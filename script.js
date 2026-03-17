170ea2fa45310d0188506abc368025fa1be2026d
OllTeeP <171400968+OllTeeP@users.noreply.github.com>
2026-03-17 22:10:17 +0100

    Create a function that returns a random answer for Rock, Paper, Scissors so the 'getComputerChoice'

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+console.log("Welcome to Rock, Paper, Scissors")
+
+
+function getComputerChoice() {
+    let random = Math.random()
+    let random_two = Math.random()
+    let choice
+    if (random > 0.5 && random_two < 0.5) {
+        choice = "Rock";
+    }
+    if (random < 0.5 && random_two > 0.5) {
+        choice = "Scissors";
+    }
+    if (random > 0.5 && random_two > 0.5 || random < 0.5 && random_two < 0.5) {
+        choice = "Paper";
+    }
+    return choice;
+}
+console.log(getComputerChoice())
